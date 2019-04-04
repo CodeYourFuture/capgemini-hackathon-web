3294f0e9f8c898e12f8cd54e331733ae05392660
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-04-04 23:40:38 +0100

    Refactored form on front page and removed redundant files

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,34 @@
 import React from "react"
+import styled from "styled-components"
+
+const Bg = styled.div`
+  min-height: 380px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+`
 
 const Form = () => (
-<iframe title="capgemini-codeyourfuture-buildathon" src="https://docs.google.com/forms/d/e/1FAIpQLSfpbfoo3yrW5hwmegNSYrbroVKG6GtHfsP3L3Wws4ZGOmlVBA/viewform?embedded=true" 
-  width="640" 
-  height="1494" 
-  frameBorder="0" 
-  marginHeight="0" 
-  marginWidth="0">
-  Loading...
-  </iframe>
+  <Bg>
+    <form action="https://docs.google.com/forms/d/e/1FAIpQLSfpbfoo3yrW5hwmegNSYrbroVKG6GtHfsP3L3Wws4ZGOmlVBA/formResponse" method="post">
+      <label>Full name *</label>
+      <input type="text" placeholder="Full name" name="entry.2092238618" required></input>
+
+      <label>Email *</label>
+      <input type="email" placeholder="Email" name="entry.1556369182" required></input>
+
+      <label>Organisation *</label>
+      <input type="text" placeholder="Organisation" name="entry.479301265" required></input>
+
+      <button type="submit">Send</button>
+    </form>
+  </Bg>
 )
 
 export default Form
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 import Form from '../components/form'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' keywords={[`code your future`, `capgemini`, `react`, `gatsby`]} />
-    <h1>Hi people</h1>
-    <h2>Welcome to your new Build-a-thon site.</h2>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1>Welcome to the CodeYourFuture Build-a-thon</h1>
+    <h2>Build for good</h2>
+    <p>To register for the build-a-thon, click on the button below</p>
+    <div style={{ maxWidth: `400px`, maxHeight: `400px` }}>
       <Form />
     </div>
-    <Link to='/page-2/'>Go to page 2</Link>
+    <Link to='/individual-apply/'>To apply as an individual</Link>
+    <Link to='/team-apply/'>To apply as a team</Link>
   </Layout>
 )
 
