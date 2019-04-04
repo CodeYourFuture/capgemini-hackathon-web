115aa1da950463ab26ebf4462d18171023f1d5ed
Atty Cronin <atty.cronin@capgemini.com>
2019-04-04 17:23:01 +0100

    Add dummy form to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import styled, { ThemeProvider } from "styled-components"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+import Form from '../components/form'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`code your future`, `capgemini`, `react`, `gatsby`]} />
+    <SEO title='Home' keywords={[`code your future`, `capgemini`, `react`, `gatsby`]} />
     <h1>Hi people</h1>
     <h2>Welcome to your new Build-a-thon site.</h2>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <Form />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to='/page-2/'>Go to page 2</Link>
   </Layout>
 )
 
